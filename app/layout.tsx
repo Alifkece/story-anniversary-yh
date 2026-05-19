0c69981ee059a5bdfd8c0b70e286d9afb522d855
AliftzyOffc <alifmuhammadalifudin@gmail.com>
2026-05-20 00:06:49 +0700

    Update metadata for anniversary celebration

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ const playfair = Playfair_Display({
 });
 
 export const metadata: Metadata = {
-  title: "Happy Valentine L",
-  description: "A special Valentine's gift for L ❤️",
+  title: "Happy Anniversary Araa",
+  description: "A special Anniversary gift for you🤍",
 };
 
 export default function RootLayout({
